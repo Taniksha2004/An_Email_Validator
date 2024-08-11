77be82619a7aba4c3dde6900e236ecc93cfa10fb
Taniksha2004 <156304371+Taniksha2004@users.noreply.github.com>
2024-08-11 20:19:54 +0530

    Add files via upload

---
+++ an email validator/script.js
@@ -0,0 +1,42 @@
+console.log("this is my script")
+let result = {
+    "tag": "",
+    "free": true,
+    "role": false,
+    "user": "tanikshamorankar60",
+    "email": "tanikshamorankar60@gmail.com",
+    "score": 0.64,
+    "state": "deliverable",
+    "domain": "gmail.com",
+    "reason": "valid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": true,
+    "did_you_mean": "",
+    "format_valid": true
+}
+
+
+submitBtn.addEventListner("click", async(e) => {
+    e.preventDefault()
+    console.log("clicked!")
+    let key = "ema_live_tIraR9QJi6hqbFY9aAxcCCTHPeEalzJT7JJxYD5L"
+    let email = document.getElementById("username").value
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key]!== " "){ 
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
+
+
+
+
